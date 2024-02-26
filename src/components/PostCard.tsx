9b4e27c6f7e5e481fd9c1fdb4d9a646a507e7311
ramalt <ramazanalltuntepe@gmail.com>
2024-02-26 13:07:22 +0300

    Removed PostCard props

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -19,23 +19,14 @@ import {
 } from "@ionic/react";
 import { chatbubbleOutline, heart, heartOutline } from "ionicons/icons";
 import React from "react";
+import { Post } from "../types/post";
 import PostDetail from "../pages/PostDetail";
 
-interface PostCardProps {
-  userName: string;
-  userImage: string;
-  commentDate: string;
-  title: string;
-  questionImg: string[];
-  description: string;
-  tags: string[];
-  heartCount: number;
-  likeCount: number;
-}
+type PostCardProps = {
+  post: Post;
+};
 
-
-const PostCard: React.FC<PostCardProps> = ({ userName, userImage, commentDate,
-  title, questionImg, description, tags, heartCount, likeCount }) => {
+const PostCard: React.FC = () => {
   return (
     <IonCard>
       <IonCardHeader>
@@ -43,57 +34,61 @@ const PostCard: React.FC<PostCardProps> = ({ userName, userImage, commentDate,
           <IonAvatar slot="start">
             <img
               alt="imgalt"
-              src={userImage}
+              src="https://i.pinimg.com/originals/32/82/21/328221397f7d20f615f9ee8cb48e4577.jpg"
             />
           </IonAvatar>
-          <IonLabel>{userName}</IonLabel>
+          <IonLabel>username_1</IonLabel>
           {/* <IonText>{new Date(post.createdDate).toLocaleTimeString()}</IonText> */}
-          <IonText>{commentDate.toString()}</IonText>
+          <IonText>4:04 PM</IonText>
         </IonItem>
 
         <IonNavLink routerDirection="forward" component={() => <PostDetail />}>
           {/* TODO: title özelliği kaldırılacak */}
-          <IonCardTitle>{title}</IonCardTitle>
+          <IonCardTitle>Lorem ipsum dolor sit amet</IonCardTitle>
         </IonNavLink>
       </IonCardHeader>
       <IonNavLink routerDirection="forward" component={() => <PostDetail />}>
         <IonGrid>
           <IonRow>
-            {questionImg.map((item, index) => (
-              <IonImg key={index} src={item}></IonImg>
-            ))}
+            <IonImg src="https://www.matematikkolay.net/wp-content/uploads/2020/01/polinom_s4.gif"></IonImg>
+            <IonImg src="https://www.matematikkolay.net/wp-content/uploads/2020/01/polinom_s1.gif"></IonImg>
           </IonRow>
         </IonGrid>
         <IonCardContent onClick={() => console.log("nav")}>
-          {description}
+          {/* {post.description.length > 200 ? (
+            <>
+              {post.description.slice(0, 100)}
+              <a>
+                <strong> ...devamını oku</strong>
+              </a>
+            </>
+          ) : (
+            post.description
+          )} */}
+          Lorem ipslor sit amet. doum dolLorem ipslor sit amet.or rem ipsumsit
+          amet, Lo
         </IonCardContent>
       </IonNavLink>
 
       <IonGrid>
         <IonRow>
-          {tags.map((item, index) => (
-            <IonChip color="warning">{"#" + item}</IonChip>
-          ))}
+          <IonChip color="warning">#KPSS</IonChip>
+          <IonChip color="tertiary">#mat</IonChip>
+          <IonChip color="secondary">#polinomlar</IonChip>
         </IonRow>
         <IonRow>
           <IonCol>
-            <IonCol>
-              <IonIcon
-                size="large"
-                color="danger"
-                icon={heart}
-                onClick={() => console.log("like")}
-              />
-              <IonLabel>{heartCount}</IonLabel>
-            </IonCol>
-            <IonCol>
-              <IonIcon
-                size="large"
-                icon={chatbubbleOutline}
-                onClick={() => console.log("like")}
-              />
-              <IonLabel>{likeCount}</IonLabel>
-            </IonCol>
+            <IonIcon
+              size="large"
+              color="danger"
+              icon={heart}
+              onClick={() => console.log("like")}
+            />
+            <IonIcon
+              size="large"
+              icon={chatbubbleOutline}
+              onClick={() => console.log("like")}
+            />
           </IonCol>
         </IonRow>
       </IonGrid>
--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -13,11 +13,11 @@ import PostCard from "../components/PostCard";
 import PostDetail from "./PostDetail";
 
 const Feed: React.FC = () => {
-
   const [loading, setLoading] = useState(false);
   const currentDate = new Date(Date.now());
-  const formattedDate = `${currentDate.toLocaleDateString('tr-TR', { dateStyle: 'medium' })} ${currentDate.toLocaleTimeString('tr-TR', { timeStyle: 'short' })}`;
-  
+  const formattedDate = `${currentDate.toLocaleDateString("tr-TR", {
+    dateStyle: "medium",
+  })} ${currentDate.toLocaleTimeString("tr-TR", { timeStyle: "short" })}`;
 
   return (
     <IonPage>
@@ -28,23 +28,7 @@ const Feed: React.FC = () => {
               routerDirection="forward"
               component={() => <PostDetail />}
             >
-              <PostCard
-                userName="Sukrubeyy"
-                userImage="https://avatars.githubusercontent.com/u/69048292?v=4"
-                commentDate={formattedDate}
-                title="Soruyu çözemedim help"
-                questionImg={[
-                  "https://www.matematikkolay.net/wp-content/uploads/2020/01/polinom_s4.gif",
-                  "https://www.matematikkolay.net/wp-content/uploads/2020/01/polinom_s1.gif"
-                ]}
-                description="Çözemiyorum HELPPPPP, kök bulamıyorum. !!!"
-                tags={[
-                  "Kpss",
-                  "YKS"
-                ]}
-                heartCount={15}
-                likeCount={24}
-              />
+              <PostCard />
             </IonNavLink>
           </IonList>
         ) : (
